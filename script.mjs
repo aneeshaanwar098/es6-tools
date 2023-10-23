1ce5dc1400c95853b0b08021fc3d794392e0ce37
aneeshaanwar098 <shehrzaadkhan@gmail.com>
2023-10-23 19:25:31 +0500

    es6 tools

---
+++ script.mjs
@@ -0,0 +1,26 @@
+let isOpen = true;//true or false boolean'name must starts from 'is'.
+console.log(isOpen);
+
+isOpen = false
+console.log(isOpen);
+
+
+
+
+let myNumbers = [1,7,3,4,5,6,7,8,9,1000] //an array variable name will end with 's'
+console.log(myNumbers);
+
+console.log(myNumbers[0]);
+console.log(myNumbers[3]);
+console.log(myNumbers[4]);
+console.log(myNumbers[5]);
+console.log(myNumbers[4]);
+
+myNumbers[0]= 9;
+myNumbers[3]=22;
+myNumbers[2]=0;
+
+console.log(myNumbers[0]);
+console.log(myNumbers[3]);
+console.log(myNumbers[2]);
+console.log(myNumbers[9]);
